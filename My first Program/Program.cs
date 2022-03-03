a80b2d334d13b0ea8829eda6a9d753678de53d51
Just-a-Random-Dev <100835122+Just-a-Random-Dev@users.noreply.github.com>
2022-03-03 13:38:48 +0400

    Add files via upload

---
+++ My first Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace My_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Random's program";
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            
+            Console.WriteLine("Hello, what's your name?");
+
+            
+            Console.ReadLine();
+            
+            Console.WriteLine("My name is Random");
+            Console.WriteLine("What's your favorite color?"); 
+            
+            Console.ReadLine();
+
+
+            Console.WriteLine("cool! Mine is Blue.");
+            Console.WriteLine("Would you like to support me? yes or yes"); 
+
+            Console.ReadLine();
+
+            Console.WriteLine("support me by following my Socials you will see all my social in the Link : https://ghostbin.com/YC0Fv ");
+
+            Console.ReadKey();
+        }
+    }
+}
